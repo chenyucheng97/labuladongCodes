a0a237a47880e9d8ebf254bc946f7953adaf29c7
陈玉程 <chenyucheng02@qutoutiao.net>
2022-03-07 20:58:05 +0800

    BM6 判断链表中是否有环

---
+++ src/nowcoder/com/top101/listnode/BM6.java
@@ -0,0 +1,16 @@
+package nowcoder.com.top101.listnode;
+
+import nowcoder.com.top101.ListNode;
+
+/**
+ * @author cyc
+ * @Description BM6 判断链表中是否有环
+ * https://www.nowcoder.com/practice/650474f313294468a4ded3ce0f7898b9?tpId=295&tags=&title=&difficulty=0&judgeStatus=0&rp=0
+ * @createTime 2022年03月07日 20:56
+ */
+public class BM6 {
+
+    public boolean hasCycle(ListNode head) {
+
+    }
+}
